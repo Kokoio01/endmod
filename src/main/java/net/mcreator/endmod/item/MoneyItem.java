2152a375c4a6c3fa8bbccab80c486afc967de1a5
Kokoio01 <73775869+Kokoio01@users.noreply.github.com>
2021-02-08 10:54:46 +0100

    Endmod
    Endmod

---
+++ src/main/java/net/mcreator/endmod/item/MoneyItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.endmod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.endmod.EndmodModElements;
+
+@EndmodModElements.ModElement.Tag
+public class MoneyItem extends EndmodModElements.ModElement {
+	@ObjectHolder("endmod:money")
+	public static final Item block = null;
+	public MoneyItem(EndmodModElements instance) {
+		super(instance, 13);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("money");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
